7001e09f373bae39706278bed7494dec65406293
lucasdelavy23 <lucasdelavy23@gmail.com>
2026-04-23 19:02:00 -0300

    Delete Modulo 1/Primeira Lista de Exercicios/Exercicio3.java
    Deletado arquivo que estava com formatação do nome errado, bagunçando a ordem da lista de exercicios.

--- Modulo 1/Primeira Lista de Exercicios/Exercicio3.java
+++
@@ -1,6 +0,0 @@
-public class Exercicio3 {                           
-    public static void main(String[] args) {
-        // Faça um programa que excreva o número 4 na tela.
-        System.out.println(4);
-    }
-}
