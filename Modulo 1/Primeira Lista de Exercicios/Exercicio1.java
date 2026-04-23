b054fc85dd7f94dadfba961dcb527d6590c11bf4
lucasdelavy23 <lucasdelavy23@gmail.com>
2026-04-23 19:01:25 -0300

    Delete Modulo 1/Primeira Lista de Exercicios/Exercicio1.java
    Deletado arquivo que estava com formatação do nome errado, bagunçando a ordem da lista de exercicios.

--- Modulo 1/Primeira Lista de Exercicios/Exercicio1.java
+++
@@ -1,5 +0,0 @@
-public class Exercicio1 {
-    public static void main(String args []){                // Faça um programa que excreva a frase "Olá Mundo!" na tela.    
-        System.out.println("Olá Mundo!");               
-    }
-}
