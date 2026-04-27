307460e4d6737a2ff12c37acbe50e37f143e5075
lucasdelavy23 <lucasdelavy23@gmail.com>
2026-04-27 19:13:53 -0300

    Ajustado nome dos exercicios 01, 02 e 03.

--- Modulo 1/Exercícios de Estruturas de Repetição/Exercicio01.java
+++ Modulo 1/Exercícios de Estruturas de Repetição/Exercicio01.java
@@ -1,4 +1,4 @@
-public class Exercicio1 {
+public class Exercicio01 {
     public static void main(String[] args) {
         // 1) Imprima na tela os números de 1 a 100
         for (int i = 0; i <= 100; i++) {
--- Modulo 1/Exercícios de Estruturas de Repetição/Exercicio03.java
+++ Modulo 1/Exercícios de Estruturas de Repetição/Exercicio03.java
@@ -1,4 +1,4 @@
-public class Exercicio3 {
+public class Exercicio03 {
     public static void main(String[] args) {
         // 3) Imprima na tela os números ímpares de 1 a 100
         for (int i = 1; i <= 100; i += 2)
